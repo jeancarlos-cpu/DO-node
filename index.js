a018abecafe4c67c82eb19c9a56a309a1751ca08
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-09-25 18:46:00 -0300

    3

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'far'
+  }
+});
+
 
 app.get('/', function (req, res) {
-  res.send('Hello Digital Ocean!')
+  // res.send('It is working!');
+  knex.select("*").from('users');
 })
 
 app.listen(80, function () {
-  console.log('Magic is happening on port 80!')
+  console.log('Magic is happening on port 80!');
 })
+
