b3385a88f39c69b252698809b595dc4b849dfb4a
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-09-25 23:05:46 -0300

    4

--- index.js
+++ index.js
@@ -5,23 +5,41 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.listen(80, function () {
+  console.log('Magic is happening on port 80!');
+})
+
 const db = require('knex')({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : '',
+    password : 'secret',
     database : 'far'
   }
 });
 
-
 app.get('/', function (req, res) {
   // res.send('It is working!');
-  res.send(db.select("*").from('users'));
+ db.select("*").from('users').then(data =>
+res.json(data[0]));
+});
+
+
+app.post('/signin', (req, res) => {
+  knex.select('email', 'hash')
+      .from('users')
+      .where('email', '=', req.body.email)
+      .then(data => {
+          const isValid = req.body.password === data[0].hash;
+          if (isValid) {
+              return res.status(200).json({status: "true"});
+          } else {
+              res.status(400).json('wrong credentials');
+          }
+      })
+      .catch(err => res.status(400).json('unable to signin'));
 })
 
-app.listen(80, function () {
-  console.log('Magic is happening on port 80!');
-})
+
 
